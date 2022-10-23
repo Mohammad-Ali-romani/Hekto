c521a61cc09b710e23b4947eb91fb0d0d38197ad
mohammad ali <mohammad.ali.romani@gmail.com>
2022-10-23 11:11:12 +0300

    add files project

---
+++ src/index.js
@@ -0,0 +1,8 @@
+import ReactDOM from 'react-dom/client';
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+const App = () => {
+  return <div>app</div>
+}
+root.render(<App />);
+
